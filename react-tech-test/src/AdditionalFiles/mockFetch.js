7d083c6b7d421147c1850483adba5568af1baa80
Jozsef Glatter <78215458+JozsefWork@users.noreply.github.com>
2022-04-04 10:59:50 +0100

    Update mockFetch.js

--- react-tech-test/src/AdditionalFiles/mockFetch.js
+++ react-tech-test/src/AdditionalFiles/mockFetch.js
@@ -5,11 +5,11 @@ export const mockFetch = async (...args) => {
     const page = args[0].split('page=')[1]
 
     const pages = new Map([
-        ['0', products.slice(0, 10)],
-        ['1', products.slice(11, 20)],
-        ['2', products.slice(21, 30)],
-        ['3', products.slice(31, 40)],
-        ['4', products.slice(41, 50)],
+        ['0', products.slice(0, 11)],
+        ['1', products.slice(11, 21)],
+        ['2', products.slice(21, 31)],
+        ['3', products.slice(31, 41)],
+        ['4', products.slice(41, 51)],
         ['5', products.slice(51, products.length)],
     ])
 
